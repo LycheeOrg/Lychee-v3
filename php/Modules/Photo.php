5550bfcd08cb5b6e4b082f708d9fcd3cf5e9a70a
Brian E Bennett <brian@ohheybrian.com>
2018-11-25 21:38:19 -0500

    Add `license` to `add` and `duplicate` methods
    Including the `license` field when a photo or video is added with
    a license included in the metadata.
    
    Copy the license field when the photo is duplicated.

--- php/Modules/Photo.php
+++ php/Modules/Photo.php
@@ -924,8 +924,6 @@ final class Photo {
 		$iptcArray = array();
 		$info      = getimagesize($url, $iptcArray);
 
-		console.log($return);
-		
 		// General information
 		$return['type']        = $info['mime'];
 		$return['width']       = $info[0];
