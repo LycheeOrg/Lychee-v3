d7c319528f0ce9b166a91cfbdfda2f2c6274fa7a
Brian E Bennett <brian@ohheybrian.com>
2018-11-28 12:09:34 -0500

    update Settings table with default_license key

---
+++ php/database/update_030206.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Update to version 3.2.6
+ */
+
+use Lychee\Modules\Database;
+use Lychee\Modules\Response;
+
+// Add license to settings
+$query  = Database::prepare($connection, "SELECT `key` FROM `?` WHERE `key` = 'default_license' LIMIT 1", array(LYCHEE_TABLE_SETTINGS));
+$result = Database::execute($connection, $query, 'update_030206', __LINE__);
+
+if ($result===false) Response::error('Could not get current license from database!');
+
+if ($result->num_rows===0) {
+
+	$query  = Database::prepare($connection, "INSERT INTO `?` (`key`, `value`) VALUES ('default_license', 'none')", array(LYCHEE_TABLE_SETTINGS));
+	$result = Database::execute($connection, $query, 'update_030206', __LINE__);
+
+	if ($result===false) Response::error('Could not add default license to database!');
+
+}
+
+// Set version
+if (Database::setVersion($connection, 'update_030206')===false) Response::error('Could not update version of database!');
