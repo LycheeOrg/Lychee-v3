16aad2bf06aa3a2a33d02c922b1ebd5f78fff2d5
rwa <apollo@rw-net.de>
2019-02-27 20:45:00 +0100

    add primary key for settings table

---
+++ php/database/update_030214.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Update to version 3.2.14
+ */
+
+use Lychee\Modules\Database;
+use Lychee\Modules\Response;
+
+$query  = Database::prepare($connection, "ALTER TABLE `?` ADD PRIMARY KEY( `key`); ", array(LYCHEE_TABLE_SETTINGS));
+$result = Database::execute($connection, $query, 'update_030214', __LINE__);
+
+if ($result===false) Response::error('Could not add primary key to settings!');
+
+// Set version
+if (Database::setVersion($connection, 'update_030214')===false) Response::error('Could not update version of database!');
