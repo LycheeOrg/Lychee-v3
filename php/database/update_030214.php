1c853d7bb29b43f78e6ec83d4c2623deb52c3ce3
rwa <apollo@rw-net.de>
2019-02-27 20:48:24 +0100

    add hint for setting key cleanup

--- php/database/update_030214.php
+++ php/database/update_030214.php
@@ -10,7 +10,7 @@ use Lychee\Modules\Response;
 $query  = Database::prepare($connection, "ALTER TABLE `?` ADD PRIMARY KEY( `key`); ", array(LYCHEE_TABLE_SETTINGS));
 $result = Database::execute($connection, $query, 'update_030214', __LINE__);
 
-if ($result===false) Response::error('Could not add primary key to settings!');
+if ($result===false) Response::error('Could not add primary key to settings! Clean up duplicate setting keys!');
 
 // Set version
 if (Database::setVersion($connection, 'update_030214')===false) Response::error('Could not update version of database!');
