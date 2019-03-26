afa13a5cc4bd53081d72694ef57afcb79c0879eb
Michael Rasmussen <mir@datanom.net>
2019-03-26 01:20:21 +0100

    Slightly improve lens info by using LensSpec tag instead of Lens tag
    Signed-off-by: Michael Rasmussen <mir@datanom.net>

--- php/Modules/Photo.php
+++ php/Modules/Photo.php
@@ -1140,7 +1140,7 @@ final class Photo {
             }
 
 			if (!empty($exif['LensInfo'])) $return['lens'] = trim($exif['LensInfo']);
-			else if(!empty($exif['Lens'])) $return['lens'] = trim($exif['Lens']);
+			else if(!empty($exif['LensSpec'])) $return['lens'] = trim($exif['LensSpec']);
 
 			// Lens field from Lightroom
 			if ($return['lens'] == '' && !empty($exif['UndefinedTag:0xA434'])) $return['lens'] = trim($exif['UndefinedTag:0xA434']);
