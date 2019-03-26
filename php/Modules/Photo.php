edb722a7ac6f20523c8d23dc10dc8f39a6d938fa
Martin Stone <martin@d7415.co.uk>
2019-03-26 19:58:50 +0000

    Move LensInfo to the bottom of the list

--- php/Modules/Photo.php
+++ php/Modules/Photo.php
@@ -1139,10 +1139,10 @@ final class Photo {
                 $return['takestamp'] = 0;
             }
 
-			if (!empty($exif['LensInfo'])) $return['lens'] = trim($exif['LensInfo']);
-			else if(!empty($exif['LensID'])) $return['lens'] = trim($exif['LensID']);
-			else if(!empty($exif['LensSpec'])) $return['lens'] = trim($exif['LensSpec']);
-			else if(!empty($exif['Lens'])) $return['lens'] = trim($exif['Lens']);
+			if (!empty($exif['LensID'])) $return['lens'] = trim($exif['LensID']);
+			else if (!empty($exif['LensSpec'])) $return['lens'] = trim($exif['LensSpec']);
+			else if (!empty($exif['Lens'])) $return['lens'] = trim($exif['Lens']);
+			else if (!empty($exif['LensInfo'])) $return['lens'] = trim($exif['LensInfo']);
 
 			// Lens field from Lightroom
 			if ($return['lens'] == '' && !empty($exif['UndefinedTag:0xA434'])) $return['lens'] = trim($exif['UndefinedTag:0xA434']);
@@ -1218,7 +1218,7 @@ final class Photo {
 			default:
 				$filepath = LYCHEE_UPLOADS_BIG;
 		}
-		
+
 		$fullfilepath = $filepath . $photo->url;
 		// Check the file actually exists
 		if (!file_exists($fullfilepath)) {
