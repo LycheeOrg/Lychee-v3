b73e0bc850d9c7df0a1a25b9bf0fac12b59849ca
kamil4 <kamil.01482@iskra.name>
2019-05-29 23:28:52 -0500

    Fixes to search
    Also search through Unsorted if logged in.
    Don't leak photos from hidden or password-protected photos albums.
    Don't leak hidden albums.
    Don't leak thumbs from password-protected albums.
    Sync the album thumb code to correctly handle thumb order, video thumbs.

--- php/helpers/search.php
+++ php/helpers/search.php
@@ -24,20 +24,23 @@ function search($term) {
 	 */
 
 	$query  = Database::prepare(Database::get(), "
-		SELECT 
-			p.id, p.title, p.tags, p.public, p.star, p.album, p.thumbUrl, p.takestamp, p.url, p.medium, p.small, p.width, p.height 
-		FROM 
-			? p 
-			join ? a on a.id = p.album
+		SELECT
+			p.id, p.title, p.tags, p.public, p.star, p.album, p.thumbUrl, p.takestamp, p.url, p.medium, p.small, p.width, p.height
+		FROM
+			? p
+			left join ? a on a.id = p.album
 		WHERE (
-				p.title LIKE '%?%' 
-				OR p.description LIKE '%?%' 
+				p.title LIKE '%?%'
+				OR p.description LIKE '%?%'
 				OR p.tags LIKE '%?%'
 			)
 			AND (
 				? = 1
-				OR a.public = 1 
-				OR p.public = 1
+				OR (
+					a.public = 1
+					AND a.visible = 1
+					AND a.password IS NULL
+				)
 			)
 	", array(LYCHEE_TABLE_PHOTOS, LYCHEE_TABLE_ALBUMS, $term, $term, $term, ((Session::isLoggedIn()) ? 1 : 0)));
 	$result = Database::execute(Database::get(), $query, __METHOD__, __LINE__);
@@ -58,17 +61,18 @@ function search($term) {
 	 */
 
 	$query  = Database::prepare(Database::get(), "
-		SELECT 
-			id, title, public, sysstamp, password 
-		FROM 
-			? 
+		SELECT
+			id, title, public, sysstamp, password
+		FROM
+			?
 		WHERE (
-				title LIKE '%?%' 
+				title LIKE '%?%'
 				OR description LIKE '%?%'
 			)
 			AND (
 				? = 1
 				OR public = 1
+				AND visible = 1
 			)
 		", array(LYCHEE_TABLE_ALBUMS, $term, $term,((Session::isLoggedIn()) ? 1 : 0)));
 	$result = Database::execute(Database::get(), $query, __METHOD__, __LINE__);
@@ -81,17 +85,20 @@ function search($term) {
 		// Turn data from the database into a front-end friendly format
 		$album = Album::prepareData($album);
 
-		// Thumbs
-		$query  = Database::prepare(Database::get(), "SELECT thumbUrl FROM ? WHERE album = '?' " . Settings::get()['sortingPhotos'] . " LIMIT 0, 3", array(LYCHEE_TABLE_PHOTOS, $album['id']));
-		$thumbs = Database::execute(Database::get(), $query, __METHOD__, __LINE__);
+		if ($album['password'] === '0' || Session::isLoggedIn()) {
+			// Thumbs
+			$query  = Database::prepare(Database::get(), "SELECT thumbUrl, type FROM ? WHERE album = '?' ORDER BY star DESC, " . substr(Settings::get()['sortingPhotos'], 9) . " LIMIT 3", array(LYCHEE_TABLE_PHOTOS, $album['id']));
+			$thumbs = Database::execute(Database::get(), $query, __METHOD__, __LINE__);
 
-		if ($thumbs===false) return false;
+			if ($thumbs===false) return false;
 
-		// For each thumb
-		$k = 0;
-		while ($thumb = $thumbs->fetch_object()) {
-			$album['thumbs'][$k] = LYCHEE_URL_UPLOADS_THUMB . $thumb->thumbUrl;
-			$k++;
+			// For each thumb
+			$k = 0;
+			while ($thumb = $thumbs->fetch_object()) {
+				$album['thumbs'][$k] = LYCHEE_URL_UPLOADS_THUMB . $thumb->thumbUrl;
+				$album['types'][$k] = $thumb->type;
+				$k++;
+			}
 		}
 
 		// Add to return
